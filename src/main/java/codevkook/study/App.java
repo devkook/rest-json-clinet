26d43c4df5871bf5527a45c8972c737c9fba5a74
devkook <devinkook@gmail.com>
2014-05-22 23:43:39 +0900

    http://examples.javacodegeeks.com/enterprise-java/rest/jersey/json-example-with-jersey-jackson/
    참고하여 작업시작 졸림 ...

--- src/main/java/codevkook/study/App.java
+++ src/main/java/codevkook/study/App.java
@@ -1,13 +1,44 @@
 package codevkook.study;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.api.json.JSONConfiguration;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        try {
+
+            ClientConfig conf = new DefaultClientConfig();
+            conf.getFeatures().get()
+
+            Client client = Client.create(conf);
+
+            WebResource webResource = client.resource("http://localhost:9090/JerseyJSONExample/rest/jsonServices/send");
+
+            ClientResponse response = webResource.accept("application/json")
+                    .type("application/json").post(ClientResponse.class, st);
+
+            if (response.getStatus() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatus());
+            }
+
+            String output = response.getEntity(String.class);
+
+            System.out.println("Server response .... \n");
+            System.out.println(output);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+
     }
 }
